a3d67d00a36b0e01b8355d4fb2bf7707a031f871
jwoonge <511sjw@naver.com>
2020-05-31 08:56:16 +0900

    some improvement

---
+++ PitchDetectModule/Accord.py
@@ -0,0 +1,341 @@
+import pretty_midi
+import csv
+from collections import Counter
+from dtw import dtw
+import numpy as np
+from matplotlib import pyplot as plt
+from LilyNotation import *
+import os
+import sys
+import subprocess
+
+class note:
+    def __init__(self, pitch, start, velocity):
+        self.pitch = pitch
+        self.time = start
+        self.velocity = velocity
+
+class accord:
+    def __init__(self):
+        self.vice = False
+        self.notes = []
+        self.beat = 0
+        self.origin_frames = -1
+        self.velocity = 0
+        self.time = 0
+        self.beat_ac = 0
+
+    def push_to_accord(self, note):
+        self.notes.append(note)
+
+    def calc_avg_velocity(self):
+        for note in self.notes:
+            self.velocity += note.velocity
+        self.velocity /= len(self.notes)
+
+    def calc_avg_start(self):
+        self.time = 0
+        for note in self.notes:
+            self.time += note.time
+        self.time /= len(self.notes)
+    
+    def calc_first_start(self):
+        self.time = self.notes[0].time
+        for note in self.notes:
+            if note.time < self.time:
+                self.time = note.time
+            
+
+class score:
+    def __init__(self, time_resolution):
+        self.accords_left = []
+        self.accords_right = []
+        self.accords = []
+        self.accord_th = 5
+        self.time_resolution = time_resolution
+
+    def push_note(self, pitch_num, s, e, velocity):
+        tnote = note(pitch_num, s, velocity)
+        if len(self.accords)==0:
+            tmp_accord = accord()
+            tmp_accord.push_to_accord(tnote)
+            self.accords.append(tmp_accord)
+
+        elif abs(tnote.time - self.accords[-1].notes[-1].time) >= self.accord_th:
+            tmp_accord = accord()
+            tmp_accord.push_to_accord(tnote)
+            self.accords.append(tmp_accord)
+
+        else:
+            self.accords[-1].push_to_accord(tnote)
+
+    def push_finished(self):
+        self.push_note(-1, self.accords[-1].notes[-1].time+320, self.accords[-1].notes[-1].time+320, -1)
+        for i in range(len(self.accords)):
+            self.accords[i].calc_avg_start()
+            #self.accords[i].calc_first_start()
+            self.accords[i].calc_avg_velocity()
+        self.mark_beat()
+        self.divide_hands()
+
+        
+
+    def divide_hands(self):
+        del self.accords[-1]
+        self.accords_left = []
+        self.accords_right = []
+        for i in range(len(self.accords)):
+            accord_it = self.accords[i]
+            accord_left = accord()
+            accord_right = accord()
+            accord_left.time = self.accords[i].time
+            accord_right.time = self.accords[i].time
+
+            for j in range(len(accord_it.notes)):
+                note_it = accord_it.notes[j]
+                if note_it.pitch <= 38:
+                    accord_left.push_to_accord(note_it)
+                else:
+                    accord_right.push_to_accord(note_it)
+
+            if len(accord_left.notes)>0:
+                self.accords_left.append(accord_left)
+                self.accords_left[-1].beat = accord_it.beat
+                if len(accord_right.notes)==0:
+                    rest = accord()
+                    rest.vice = True
+                    rest.pitch = -1
+                    rest.beat = accord_it.beat
+                    self.accords_right.append(rest)
+            if len(accord_right.notes)>0:
+                self.accords_right.append(accord_right)
+                self.accords_right[-1].beat = accord_it.beat
+                if len(accord_left.notes)==0:
+                    rest = accord()
+                    rest.vice = True
+                    rest.pitch = -1
+                    rest.beat = accord_it.beat
+                    self.accords_left.append(rest)
+
+    def get_interval(self, accords):
+        pitches_starts = [[] for i in range(88)]
+        for i in range(len(accords)-1):
+            for j in range(len(accords[i].notes)):
+                pitches_starts[accords[i].notes[j].pitch-8].append(accords[i].notes[j].time)
+        pitches_diff = []
+        for i in range(88):
+            for j in range(len(pitches_starts[i])-1):
+                diff = pitches_starts[i][j+1] - pitches_starts[i][j]
+                pitches_diff.append(diff)
+
+        distances = []
+        ranges = [0.01*x for x in range(800,3000)]
+        for interval in ranges:
+            distance = 0
+            for diff in pitches_diff:
+                left = abs(diff / interval - round(diff/interval))
+                distance += left / np.sqrt(interval)
+            distances.append(distance)
+        interval = np.argmin(distances)/100 + 8
+        return interval
+
+    def mark_beat(self):
+        ############ 
+        n = 4
+        temp = int(self.accords[-1].time/n) #+1
+        bars = [[] for x in range(n+50)]
+        indexes = [[] for x in range(n+50)]
+        for i in range(len(self.accords)):
+            bars[int(self.accords[i].time/temp)].append(self.accords[i])
+            indexes[int(self.accords[i].time/temp)].append(i)
+        while(True):
+            if len(indexes[-1])>0:
+                break
+            else:
+                del bars[-1]
+                del indexes[-1]
+
+        for index_bar in range(len(bars)):
+            interval = self.get_interval(bars[index_bar])
+            print('interval:',interval)
+            for index_accord in range(len(bars[index_bar])-1):
+                frames = bars[index_bar][index_accord+1].time - bars[index_bar][index_accord].time
+                #print(frames, end=" : ")
+                beat = self.get_near_beat(frames/interval)
+                if not beat==0:
+                    beat = int(16/beat)
+                #print(beat, end="  ")
+                self.accords[indexes[index_bar][index_accord]].beat = beat
+            if index_bar < len(bars)-1:
+                frame_last = bars[index_bar+1][0].time - bars[index_bar][-1].time
+                beat_last = self.get_near_beat(frame_last/interval)
+                if not beat_last == 0:
+                    beat_last = int(16/beat_last)
+                self.accords[indexes[index_bar][-1]].beat = beat_last
+            else:
+                self.accords[indexes[index_bar][-1]].beat = 1
+
+            #print("\n-----------------------\n")
+        #for i in range(len(self.accords)):
+        #    print(self.accords[i].beat, end=" ")
+        self.interval = 23
+
+    def get_near_beat(self,frame):
+        if frame > 32:
+            return round(frame/16)*16
+        elif frame > 16:
+            return round(frame/8)*8
+        elif frame > 8:
+            return round(frame/4)*4
+        elif frame > 4:
+            return round(frame/2)*2
+        elif frame <= 4 and frame>=0.75:
+            return round(frame)
+        elif frame<0.75 and frame>0.25:
+            return 0.5
+        else:
+            return 0
+            #return round(frame)
+
+        
+    def make_score(self, filename='test', title='test'):
+        self.push_finished()
+
+        self.bar = 4
+        self.key = 'C'
+        lily_string = lily_notation(self.accords_left, self.accords_right, self.bar, self.key, title, midi=0)
+        try:
+            f=open(filename+".ly","w")
+            f.write(lily_string)
+            f.close()
+        except:
+            print("except")
+        #p = subprocess.Popen(filename+".ly", shell=True).wait()
+        #os.remove(filename+".ly")
+        #os.remove(filename+".log")
+
+
+    def make_midi(self, filename='output'):
+        self.push_finished()
+
+        midi = pretty_midi.PrettyMIDI(initial_tempo = 120)
+        piano = pretty_midi.Instrument(program=1)
+
+        for i in range(len(self.accords)):
+            accord_it = self.accords[i]
+            start_time = accord_it.time * self.time_resolution
+            end_time = start_time + 0.25
+            velocity = accord_it.velocity
+            for j in range(len(accord_it.notes)):
+                note_it = accord_it.notes[j]
+                midi_note = pretty_midi.Note(pitch=int(note_it.pitch+21), start=start_time, end=end_time, velocity=int(velocity))
+                piano.notes.append(midi_note)
+        
+        midi.instruments.append(piano)
+        midi.write(filename+'.mid')
+        print("midi generated")
+
+    def make_midi_beat(self, filename='output'):
+
+        self.push_finished()
+
+        midi = pretty_midi.PrettyMIDI(initial_tempo = 120)
+        piano = pretty_midi.Instrument(program=1)
+
+        start_time = 3
+        for i in range(len(self.accords)):
+            accord_it = self.accords[i]
+            start_time = start_time + accord_it.beat*self.interval*self.time_resolution
+            end_time = start_time + 0.25
+            #velocity = accord_it.velocity
+            velocity=100
+            for j in range(len(accord_it.notes)):
+                note_it = accord_it.notes[j]
+                midi_note = pretty_midi.Note(pitch=int(note_it.pitch+21), start=start_time, end=end_time, velocity=int(velocity))
+                piano.notes.append(midi_note)
+        
+        midi.instruments.append(piano)
+        midi.write(filename+'.mid')
+        print("midi generated")
+    
+    def make_csv(self,filename) :
+        self.note_stack = sorted(self.note_stack, key=lambda x:x.start)
+        csv.register_dialect(
+            'mydialect',
+            delimiter = ',',
+            quotechar = '"',
+            doublequote = True,
+            skipinitialspace = True,
+            lineterminator = '\r\n',
+            quoting = csv.QUOTE_MINIMAL)
+        with open(filename, 'w', newline='') as mycsvfile:
+            thedatawriter = csv.writer(mycsvfile, dialect='mydialect')
+            thedatawriter.writerow([self.time_resolution])
+            for row in self.note_stack:
+                thedatawriter.writerow([row.pitch,row.start,row.end,row.velocity])
+    
+    def read_csv(self, filename) :
+        csv.register_dialect(
+            'mydialect',
+            delimiter = ',',
+            quotechar = '"',
+            doublequote = True,
+            skipinitialspace = True,
+            lineterminator = '\r\n',
+            quoting = csv.QUOTE_MINIMAL)
+
+        with open(filename, 'r') as mycsvfile:
+            thedata = csv.reader(mycsvfile, dialect='mydialect')
+            i=0
+            for row in thedata:
+                if(i==0):
+                    self.time_resolution = float(row[0])
+                    i+=1
+                else :
+                    pitch = int(row[0])
+                    start = int(row[1])
+                    end = int(row[2])
+                    velocity = int(row[3])
+                    self.push_note(pitch, start, end, velocity)
+
+if __name__=='__main__':
+    
+    #result = score(0)
+    #result.read_csv('results/아르카나.csv')
+    #result.make_midi_beat('results/작은별비트')
+    
+
+    '''
+    result = score(0.05)
+    result.push_note(57,102,4,100) #2
+    #result.push_note(57,20,4,100)
+    result.push_note(57,139,4,100) #1
+    result.push_note(57,160,4,100) #1
+    result.push_note(57,179,4,100) #1
+    result.push_note(57,199,4,100) #0
+    result.push_note(57,205,4,100) #0
+    result.push_note(57,211,4,100) #0.5
+    result.push_note(57,220,4,100) #2
+    result.push_note(57,264,4,100) #1
+    result.push_note(67,282,4,100)
+    result.make_midi_beat("results/test")
+    # 답 : 2 1 1 1 1 2 1
+    '''
+    
+
+
+    result = score(0.05)
+    result.push_note(55,102,4,100) #2
+    #result.push_note(57,20,4,100)
+    result.push_note(57,139,4,100) #1
+    result.push_note(27,160,4,100) #1
+    result.push_note(57,179,4,100) #1
+    result.push_note(59,199,4,100) #0.5
+    result.push_note(51,200,4,100)
+    result.push_note(61,211,4,100) #0.5
+    result.push_note(63,220,4,100) #2
+    result.push_note(57,264,4,100) #1
+    result.push_note(67,282,4,100)
+    result.make_score()
+    
+    
---
+++ PitchDetectModule/PitchDetection.py
@@ -0,0 +1,304 @@
+import os
+import sys
+sys.path.append(os.path.dirname(__file__))
+
+from Sound_ds import sound
+import math
+import numpy as np
+from scipy import signal
+from scipy import fftpack
+from scipy.ndimage import filters
+
+import time
+import matplotlib.pyplot as plt
+import seaborn as sns
+from celluloid import Camera
+import csv
+from Accord import score
+
+def Show_Distribution(input_spec):
+    maxes = []
+    for i in range(len(input_spec)):
+        maxes.append(max(input_spec[i]))
+    maxZ = int(max(maxes))
+    distribution = [0 for i in range(maxZ+5)]
+    for i in range(len(input_spec)):
+        for j in range(len(input_spec[i])):
+            distribution[int(input_spec[i][j])] += 1
+    
+    plt.plot(distribution)
+    plt.plot([0,len(distribution)],[0,0])
+    plt.show()
+
+def Show_Animation(input_spec,input_interval) :
+
+    # set figure and axes
+    f, ax = plt.subplots(figsize=(15,5))
+    camera = Camera(f)
+    xlabel = [i for i in range(88)]
+    ax.xaxis.set_ticks(np.arange(0, 88, 1))
+    for i in range(len(input_spec)) :
+        ax.plot(xlabel,input_spec[i])
+        camera.snap()
+        f
+
+    animation = camera.animate(interval= input_interval, repeat=True)
+    animation.save('animation.mp4')
+
+def Show_Spectrogram(input_Spec, vmin=0, vmax=150, input_list_c = [], input_list_r = [], transpose = True):
+    ''' input : Spectogram [] '''
+    plt.figure(figsize=(35,3))
+    if transpose:
+        ax = sns.heatmap(np.transpose(input_Spec), vmin=vmin,vmax=vmax,cmap=sns.cm.rocket)
+    else:
+        ax = sns.heatmap(input_Spec, vmin=vmin,vmax=vmax,cmap=sns.cm.rocket)
+    ax.invert_yaxis()
+    maxValue = len(input_Spec[0])
+    minValue = 0
+    for i in range(len(input_list_c)):
+        plt.plot([input_list_c[i],input_list_c[i]],[minValue,maxValue],'c')
+    for i in range(len(input_list_r)):
+        plt.plot([input_list_r[i],input_list_r[i]],[minValue,maxValue],'r')
+
+    plt.show()
+
+
+
+def get_key_freq(key_count=88):
+    center_a = 48
+    key_freq = {i:0 for i in range(key_count)}
+    diff = [i-center_a for i in range(key_count)]
+
+    for i in range(len(key_freq)):
+        key_freq[i] = 440 * math.pow(2, diff[i] * (1/12.0))
+    output_dict = {v:k for k,v in key_freq.items()}
+
+    return output_dict
+
+def get_freq_dict(freq_range, tuning_rate=1):
+    dictionary = [0 for i in range(freq_range)]
+    for i in range(len(dictionary)):
+        dictionary[i] = get_near_pitch_num(round(i*tuning_rate))
+    return dictionary
+
+def get_near_pitch_num(freq):
+    keys = np.array(list(get_key_freq()))
+    if freq <= max(keys) and freq >= min(keys):
+        idx = np.abs(keys - freq)
+        idx = idx.argmin()
+    else:
+        idx = -1
+    return idx
+
+
+class pd_processor:
+    def __init__(self):
+        self.dict = get_key_freq()
+        self.key_freq = list(self.dict)
+        self.set_thresholds()
+
+
+    def set_thresholds(self):
+        self.spec_th = 1
+        # octave reduce
+        self.octave_reduce_rate_r = 0.3             # same octave high pitch decrease-rate
+        self.octave_reduce_rate_l = 0.10               # same octave low pitch decrease-rate
+        self.misol_reduce_rate = 0.5                # +4 +7 pitch decrease-rate
+
+    def do(self, input_pcm):
+        self.sample_rate = input_pcm.sample_rate
+        self.spec = self.get_spectrogram(input_pcm)
+        self.global_max = np.max(np.max(self.spec, axis=0),axis=0)
+        self.result = score(self.time_resolution)
+        self.detect_pitch()
+        return self.result
+
+    def detect_pitch_Muitos(self) :
+        freq_peaks = []
+        frame_peaks = []
+        freq_convex = []
+        valid_peaks = []
+        start_ends = []
+        for i in range(88) :
+            convex, _ = signal.find_peaks(max(self.spec[:,i])-self.spec[:,i],prominence=1)
+            peaks, _ = signal.find_peaks(self.spec[:,i],prominence= 450/(i+40))
+
+            freq_peaks.append(peaks)
+            freq_convex.append(convex)
+        
+        for i in range(88):
+            valid_peak = []
+            start_end = []
+            for j in range(len(freq_peaks[i])):
+                for k in range(len(freq_convex[i])-1):
+                    if freq_peaks[i][j] in range(freq_convex[i][k], freq_convex[i][k+1]):
+                        if freq_convex[i][k+1]-freq_convex[i][k] > 20:
+                            valid_peak.append(freq_peaks[i][j])
+                            start_end.append([freq_convex[i][k],freq_convex[i][k+1]])
+            valid_peaks.append(valid_peak)
+            start_ends.append(start_end)
+        freq_peaks = valid_peaks
+
+        '''
+        for i in range(88) :
+            if(max(self.spec[:,i]) > 50) :
+                plt.plot(self.spec[:,i])
+                plt.plot(freq_convex[i],self.spec[:,i][freq_convex[i]],'or')
+                plt.plot(freq_peaks[i],self.spec[:,i][freq_peaks[i]],'xb')
+                plt.title(str(i+1))
+                plt.show()
+        '''
+
+        for i in range(len(self.spec)):
+            peaks,_ = signal.find_peaks(self.spec[i,:])
+            frame_peaks.append(peaks)
+        '''
+        union_peaks = []
+        for i in range(88) :
+            union_peaks = np.union1d(union_peaks,freq_peaks[i])'''
+
+
+
+        
+        coord_list = []
+        for i in range(88):
+            for j in range(len(frame_peaks)):
+
+                if i in frame_peaks[j] and j in freq_peaks[i] :
+                    coord_list.append([j,i])
+
+        #print(coord_list)
+
+        self.octave_decrease()
+        count = 0
+        for i in range(len(coord_list)) :
+            if self.spec[coord_list[i][0]][coord_list[i][1]] > 1 :
+                    count+=1
+                    self.result.push_note(coord_list[i][1]+8,coord_list[i][0],coord_list[i][0]+40,100)
+   
+
+        print("note count = ",count)
+
+
+    def detect_pitch(self):
+        frame_energy = np.sum(self.spec, axis=1)
+        total_concaves, _ = signal.find_peaks(frame_energy, distance=8, prominence=3, height=10)
+        
+        valid_peaks = []
+        start_ends = []
+        for i in range(88):
+            convex, _ = signal.find_peaks(max(self.spec[:,i])-self.spec[:,i], prominence=2)
+            peaks, _ = signal.find_peaks(self.spec[:,i], prominence = 450/(i+40))
+
+            valid_peak = []
+            start_end = []
+            for j in range(len(peaks)):
+                for k in range(len(convex)-1):
+                    if peaks[j] in range(convex[k], convex[k+1]):
+                        if convex[k+1]-convex[k] > 20:
+                            valid_peak.append(peaks[j])
+                            start_end.append([convex[k], convex[k+1]])
+                            break
+            valid_peaks.append(valid_peak)
+            start_ends.append(start_end)
+        
+        pitch_map = np.zeros(np.shape(self.spec))
+
+        for i in range(88):
+            for j in range(len(start_ends[i])):
+                for k in range(len(total_concaves)):
+                    if total_concaves[k] in range(start_ends[i][j][0], start_ends[i][j][1]):
+                        pitch_map[total_concaves[k]][i] = 1
+                        break
+
+        self.octave_decrease()
+        count = 0
+        for frame in range(len(self.spec)):
+            for freq in range(1,87):
+                if pitch_map[frame][freq] != 0 and self.spec[frame][freq]>1:
+                    if self.spec[frame][freq-1] < self.spec[frame][freq] and self.spec[frame][freq+1]<self.spec[frame][freq]:
+                        self.result.push_note(freq+8, frame, frame+40, 100)
+                        count += 1
+        print("num_note : ", count)
+
+
+    def get_tuning_rate(self, spec):
+        max_freq = np.argmax(spec[:,50:150])%(100)+50
+        #max_freq = np.argmax(spec[:][100:])%(np.shape(spec)[1]-100)+100
+        near_freq = self.key_freq[get_near_pitch_num(max_freq)]
+        print("\t max_freq : ",max_freq)
+        print("\t near_freq: ",near_freq)
+        tuning_rate = near_freq/max_freq
+        print("\t tuning_rate: ",tuning_rate)
+        return tuning_rate
+
+    def get_spectrogram(self, pcm):
+        start_t = time.time()
+        print("Calc Spectrogram...")
+        freq_resolution = self.key_freq[1]-self.key_freq[0]
+        n = int(pcm.sample_rate/freq_resolution/4)
+        overlap_rate = 0.95
+        time_resolution_rate = 1-overlap_rate
+        self.time_resolution = n / self.sample_rate * time_resolution_rate
+        freq, t, Zxx = signal.stft(pcm.data, self.sample_rate, nperseg = n, noverlap = int(n*overlap_rate))
+        Zxx = np.transpose(np.abs(Zxx))
+        Zxx = np.where(Zxx<self.spec_th, 0, Zxx)
+        del freq
+        del t
+
+        tuning_rate = self.get_tuning_rate(Zxx)
+        self.dict = get_freq_dict(np.shape(Zxx)[1], tuning_rate*4)
+        end_t = time.time()
+        print("\t Fourier Transform Complete", round(end_t-start_t,2),"sec")
+
+        start_t = time.time()
+        spec = np.zeros((np.shape(Zxx)[0],88))
+
+        for freq in range(len(Zxx[0])):
+            pitch = self.dict[freq]
+            spec[:,pitch] = spec[:,pitch] + Zxx[:,freq]
+        spec = np.sqrt(spec)
+        end_t = time.time()
+        print('\t Generating freq-to-pitch Complete', round(end_t-start_t,2),"sec")
+
+        print("\t Generating Spectrogram Complete")
+        print("\t sec per frame : ", self.time_resolution)
+        return spec
+        
+    def octave_decrease(self):
+        decrease_value = np.zeros(np.shape(self.spec))
+        for pitch_origin in range(88):
+            for pitch_to_dec in range(88):
+                reduce_rate = 0
+                octave_dif = pitch_to_dec - pitch_origin
+
+                if not octave_dif == 0:
+                    if octave_dif%12 == 0:
+                        reduce_rate = 1
+                    elif octave_dif%12 == 4 or octave_dif%12 == 7:
+                        reduce_rate = self.misol_reduce_rate
+
+                    if octave_dif < 0:
+                        reduce_rate *= self.octave_reduce_rate_l
+                    elif octave_dif > 0:
+                        reduce_rate *= self.octave_reduce_rate_r
+
+                decrease_value[:,pitch_to_dec] = decrease_value[:,pitch_to_dec] + self.spec[:,pitch_origin]*reduce_rate
+        
+        self.spec = self.spec - decrease_value
+        self.spec = np.where(self.spec < 0, 0, self.spec)
+
+
+
+
+
+
+pdp = pd_processor()
+#test_sound = sound('https://www.youtube.com/watch?v=Hf2MFBz4S_g') #라캄파넬라
+#test_sound = sound('https://www.youtube.com/watch?v=22jE6FdYjxE') #왕벌
+#test_sound = sound('https://www.youtube.com/watch?v=6vo66K06wFU') #아르카나
+#test_sound = sound('https://www.youtube.com/watch?v=w-4xH2DLv8M') # 작은별
+test_sound = sound('https://www.youtube.com/watch?v=cqOY7LF_QrY') #관짝
+result = pdp.do(test_sound)
+#result.make_midi()
+result.make_score()
